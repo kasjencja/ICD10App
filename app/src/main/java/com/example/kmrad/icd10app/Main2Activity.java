1d0df2565e34e8ef63238720fa3cd40f4e13bb17
kasjencja <kasjencja@gmail.com>
2018-04-18 16:48:20 +0200

    lab

--- app/src/main/java/com/example/kmrad/icd10app/Main2Activity.java
+++ app/src/main/java/com/example/kmrad/icd10app/Main2Activity.java
@@ -20,7 +20,7 @@ public class Main2Activity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/com/example/kmrad/icd10app/MainActivity.java
+++ app/src/main/java/com/example/kmrad/icd10app/MainActivity.java
@@ -4,16 +4,37 @@ import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +42,18 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> codes = new ArrayList<>();
 
     private CodesAdapter codesAdapter;
-
+    private OkHttpClient client;
 
     @OnClick(R.id.button_szukaj)
     void OnClick(){
-        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-        startActivity(intent);
+        String query = editDiagnosis.getText().toString();
+        getData(query);
+        //Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        //startActivity(intent);
     }
+
+    @BindView(R.id.info)
+    TextView info;
 
     @BindView(R.id.edit_text_diagnosis)
     EditText editDiagnosis;
@@ -60,10 +86,55 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    private void createClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.HEADERS);
+        client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+    }
+
+    private Request createRequest(String url) {
+        return new Request.Builder()
+                .url(BASE_URL + url)
+                .build();
+    }
+
+    private void getData(String query) {
+        try {
+            URL url = new URL(BASE_URL+query);
+            InputStream openStream = url.openConnection().getInputStream();
+            //URLConnection conn = url.openConnection();
+            //BufferedReader br = new BufferedReader(
+            //        new InputStreamReader(conn.getInputStream())); //tutaj!
+
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                inputLine += br.readLine();
+            }
+            br.close();
+            info.setText(inputLine);
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        createClient();
     }
 }
